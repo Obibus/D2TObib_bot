9c1fdb2e4a6e078717acb47d937a00d01c4011ae
Maks Bidziura <maksbidzura@gmail.com>
2024-09-30 19:07:06 +0300

    pererobuv logiku radi portiv

--- D2TObib_bot.py
+++ D2TObib_bot.py
@@ -3,10 +3,10 @@ import discord
 from discord.ext import commands
 import asyncio
 import threading
-# from config import telegram_bot_token, discord_bot_token, telegram_chat_id
+from flask import Flask
 import os
 
-# Використовуйте змінні оточення для токенів та інших конфіденційних даних
+# Використовуємо змінні оточення для токенів та інших конфіденційних даних
 telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
 discord_bot_token = os.getenv('DISCORD_BOT_TOKEN')
 telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
@@ -20,6 +20,9 @@ discord_bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Telegram Bot
 telegram_bot = telebot.TeleBot(telegram_bot_token)
+
+# Flask для відкриття порту
+app = Flask(__name__)
 
 # Telegram бот: команда /start
 @telegram_bot.message_handler(commands=['start'])
@@ -45,11 +48,20 @@ def run_telegram_bot():
 async def run_discord_bot():
     await discord_bot.start(discord_bot_token)
 
+# Flask маршрут для перевірки статусу сервера
+@app.route('/')
+def home():
+    return "Бот працює!"
+
 # Основна функція для запуску обох ботів
 if __name__ == "__main__":
     # Запускаємо Telegram бота в окремому потоці
     telegram_thread = threading.Thread(target=run_telegram_bot)
     telegram_thread.start()
+
+    # Запускаємо Flask для прослуховування порту
+    port = int(os.environ.get('PORT', 5000))  # Render надасть порт через змінну PORT
+    app.run(host='0.0.0.0', port=port)
 
     # Запускаємо Discord бота в основному потоці (асинхронно)
     asyncio.run(run_discord_bot())
