3ece732e20a47675546d42f3713641334fe05110
Maks Bidziura <maksbidzura@gmail.com>
2024-09-30 18:10:59 +0300

    -git ignore

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+# Використовуйте змінні оточення для токенів та інших конфіденційних даних
+telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+discord_bot_token = os.getenv('DISCORD_BOT_TOKEN')
+telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
