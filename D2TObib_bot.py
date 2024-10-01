2ddf48898cc58804580da76855ccbbcfb5c463c4
Maks Bidziura <maksbidzura@gmail.com>
2024-10-01 10:07:17 +0300

    +webhook like var

--- D2TObib_bot.py
+++ D2TObib_bot.py
@@ -1,15 +1,14 @@
 import telebot
 import discord
 from discord.ext import commands
-import asyncio
-import threading
-from flask import Flask
 import os
+from flask import Flask, request
 
 # Використовуємо змінні оточення для токенів та інших конфіденційних даних
 telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
 discord_bot_token = os.getenv('DISCORD_BOT_TOKEN')
 telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
+webhook_url = os.getenv('WEBHOOK_URL')
 
 # Імпортуємо Intents для Discord
 intents = discord.Intents.default()
@@ -40,13 +39,13 @@ async def send_to_telegram(ctx, *, message):
     telegram_bot.send_message(telegram_chat_id, message)
     await ctx.send('Повідомлення надіслано в Telegram!')
 
-# Функція для запуску Telegram бота у фоновому потоці
-def run_telegram_bot():
-    telegram_bot.polling(none_stop=True)
-
-# Функція для запуску Discord бота
-async def run_discord_bot():
-    await discord_bot.start(discord_bot_token)
+# Flask маршрут для вебхука
+@app.route('/webhook', methods=['POST'])
+def telegram_webhook():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    telegram_bot.process_new_updates([update])
+    return '!', 200
 
 # Flask маршрут для перевірки статусу сервера
 @app.route('/')
@@ -55,13 +54,10 @@ def home():
 
 # Основна функція для запуску обох ботів
 if __name__ == "__main__":
-    # Запускаємо Telegram бота в окремому потоці
-    telegram_thread = threading.Thread(target=run_telegram_bot)
-    telegram_thread.start()
+    # Встановлення вебхука для Telegram
+    telegram_bot.remove_webhook()
+    telegram_bot.set_webhook(url=f'{webhook_url}/webhook')
 
     # Запускаємо Flask для прослуховування порту
     port = int(os.environ.get('PORT', 5000))  # Render надасть порт через змінну PORT
     app.run(host='0.0.0.0', port=port)
-
-    # Запускаємо Discord бота в основному потоці (асинхронно)
-    asyncio.run(run_discord_bot())
